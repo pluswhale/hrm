95d94303d29994c61db1f84589df7dc407cdd0b7
Egor Dultsev <listenmy@yandex.ru>
2024-05-07 01:37:38 +0300

    add fonts

--- src/pages/employee-list/employees-list.tsx
+++ src/pages/employee-list/employees-list.tsx
@@ -1,34 +1,24 @@
-import { FC, ReactElement, useState } from "react";
-import SwitchTab from "../../shared/components/switch-tab/switch-tab";
-import { EmployeesDataContainer } from "../../features/employees-list-data-container";
-import { Filter } from "../../features/filter";
-import { employeeListData, filterSet } from "./constants";
+import { FC, ReactElement, useState } from 'react';
+import SwitchTab from '../../shared/components/switch-tab/switch-tab';
+import { EmployeesDataContainer } from '../../features/employees-list-data-container';
+import { Filter } from '../../features/filter';
+import { employeeListData, filterSet } from './constants';
 
 import style from './employees-list.module.scss';
-import { DefaultContentWrapper } from "entities/default-content-wrapper/default-content-wrapper";
+import { DefaultContentWrapper } from 'entities/default-content-wrapper/default-content-wrapper';
 
 export const EmployeesList: FC = (): ReactElement => {
     const [activeTab, setActiveTab] = useState<number>(0);
 
-    const tabs = [{ label: "Текущие сотрудники" }, { label: "Бывшие сотрудники" }];
+    const tabs = [{ label: 'Текущие сотрудники' }, { label: 'Бывшие сотрудники' }];
 
     return (
         <DefaultContentWrapper>
-            <SwitchTab
-                tabs={tabs}
-                onTabClick={setActiveTab}
-                activeTab={activeTab}
-                design="default"
-            />
+            <SwitchTab tabs={tabs} onTabClick={setActiveTab} activeTab={activeTab} design="default" />
             <div className={style.main_content}>
-                <EmployeesDataContainer employees={employeeListData?.employees}/>
-                <Filter
-                    title="Поиск кандидата"
-                    filterSet={filterSet}
-                    onClickSearch={() => console.log('заглушка')}
-                />
+                <EmployeesDataContainer employees={employeeListData?.employees} />
+                <Filter title="Поиск кандидата" filterSet={filterSet} onClickSearch={() => console.log('заглушка')} />
             </div>
         </DefaultContentWrapper>
-
     );
 };
--- src/pages/vacancies-candidate/vacancies-candidate.tsx
+++ src/pages/vacancies-candidate/vacancies-candidate.tsx
@@ -1,9 +1,34 @@
-const VacanciesCandidate = () => {
+import { DefaultContentWrapper } from 'entities/default-content-wrapper/default-content-wrapper';
+import { Button } from 'shared/components/button/button';
+import { HorizontalNavigation } from 'shared/components/horizontal-navigation';
 
+import styles from './vacancies-candidate.module.scss';
+
+const VacanciesCandidate = () => {
+    const navigation = [
+        {
+            title: 'Активные вакансии',
+            url: '/vacancies',
+        },
+        {
+            title: 'Название вакансии',
+            url: '/vacancies',
+        },
+        {
+            title: 'Рыбина Анастасия',
+            url: '/vacancies',
+        },
+    ];
 
     return (
-        <></>
-    )
-}
+        <DefaultContentWrapper>
+            <div className={styles.vacancy_navigation}>
+                <HorizontalNavigation navigation={navigation} />
+                <Button text="Редактировать" type="default_bg_white" />
+            </div>
+        </DefaultContentWrapper>
+    );
+};
 
 export default VacanciesCandidate;
+
