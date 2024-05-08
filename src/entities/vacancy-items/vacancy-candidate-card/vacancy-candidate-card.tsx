4a4821b2884fc97f4a2977f8b9d15281ded9f10e
Egor Dultsev <listenmy@yandex.ru>
2024-05-08 12:25:39 +0300

    add canban to candidates

--- src/entities/vacancy-items/vacancy-candidate-card/vacancy-candidate-card.tsx
+++ src/entities/vacancy-items/vacancy-candidate-card/vacancy-candidate-card.tsx
@@ -1,32 +1,37 @@
 import { FC, ReactElement } from 'react';
 import { VacancyCandidateCardProps } from './types';
-
 import mockAvatar from '../../../assets/Ellipse 1.svg';
-
 import { Link, useParams } from 'react-router-dom';
+import { Draggable } from 'react-beautiful-dnd';
 
 import styles from './vacancy-card.module.scss';
 
-export const VacancyCandidateCard: FC<VacancyCandidateCardProps> = ({ candidate }): ReactElement => {
+export const VacancyCandidateCard: FC<VacancyCandidateCardProps> = ({ candidate, index }): ReactElement => {
     const { id: vacancyId } = useParams();
 
     return (
-        <div className={styles.vacancy_candidate_card}>
-            <div className={styles.vacancy_candidate_card__container}>
-                <img
-                    className={styles.vacancy_candidate_card__avatar}
-                    src={candidate.avatar || mockAvatar}
-                    alt="candidate avatar"
-                />
-                <div className={styles.vacancy_candidate_card__title_and_info}>
-                    <Link to={`/vacancies/${vacancyId}/${candidate.id}`}>
-                        <h5 className={styles.vacancy_candidate_card__title}>{candidate.name}</h5>
-                    </Link>
-                    <span className={styles.vacancy_candidate_card__info}>{candidate.preferredIncome}</span>
-                    <span className={styles.vacancy_candidate_card__info}>{candidate.experience}</span>
+        <Draggable key={candidate.id} draggableId={String(candidate.id)} index={index}>
+            {(provided) => (
+                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <div className={styles.vacancy_candidate_card}>
+                        <div className={styles.vacancy_candidate_card__container}>
+                            <img
+                                className={styles.vacancy_candidate_card__avatar}
+                                src={candidate.avatar || mockAvatar}
+                                alt="candidate avatar"
+                            />
+                            <div className={styles.vacancy_candidate_card__title_and_info}>
+                                <Link to={`/vacancies/${vacancyId}/${candidate.id}`}>
+                                    <h5 className={styles.vacancy_candidate_card__title}>{candidate.name}</h5>
+                                </Link>
+                                <span className={styles.vacancy_candidate_card__info}>{candidate.preferredIncome}</span>
+                                <span className={styles.vacancy_candidate_card__info}>{candidate.experience}</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </Draggable>
     );
 };
 
--- src/features/vacancy-candidates/vacancy-candidates.tsx
+++ src/features/vacancy-candidates/vacancy-candidates.tsx
@@ -7,8 +7,10 @@ import { PopupWithDarkOverlay } from 'shared/components/portal/popup-with-dark-o
 import { VacancyRecruitingFunnel } from 'entities/vacancy-items/vacancy-modals/vacancy-recruting-funnel/vacancy-recruiting-funnel';
 
 import styles from './vacancy-candidates.module.scss';
+import { useNavigate } from 'react-router';
 
 export const VacancyCandidates: FC<VacancyCandidatesProps> = ({ candidateRows }): ReactElement => {
+    const navigate = useNavigate();
     const [isModalRecruitingFunnelOpened, setIsModalRecruitingFunnelOpened] = useState<boolean>(false);
 
     const candidatesCount = candidateRows ? candidateRows.reduce((acc, row) => row.count + acc, 0) : 0;
@@ -21,6 +23,10 @@ export const VacancyCandidates: FC<VacancyCandidatesProps> = ({ candidateRows })
         setIsModalRecruitingFunnelOpened(false);
     };
 
+    const onNavigateToCreateCandidate = () => {
+        navigate('/create/candidate');
+    };
+
     return (
         <>
             <div className={styles.vacancy_candidates}>
@@ -28,12 +34,11 @@ export const VacancyCandidates: FC<VacancyCandidatesProps> = ({ candidateRows })
                     <span className={styles.vacancy_candidates__count}>Кандидаты - {candidatesCount}</span>
                     <div className={styles.vacancy_candidates__buttons}>
                         <Button onClick={onOpenModalRecruitingFunnel} text="Воронка рекрутинга" view="default_bg" />
-                        <Button text="Добавить кандидата" view="default_bg" />
+                        <Button onClick={onNavigateToCreateCandidate} text="Добавить кандидата" view="default_bg" />
                     </div>
                 </div>
                 <div className={styles.vacancy_candidates__list}>
-                    {candidateRows &&
-                        candidateRows.map((candidateRow) => <VacancyCandidatesBlock candidatesRow={candidateRow} />)}
+                    <VacancyCandidatesBlock />
                 </div>
             </div>
             <PopupWithDarkOverlay onClose={onCloseModalRecruitingFunnel} isOpened={isModalRecruitingFunnelOpened}>
