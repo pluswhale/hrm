c93a593edf08697cd95f49ade58109269a2ac3a6
Egor Dultsev <listenmy@yandex.ru>
2024-05-08 22:43:43 +0300

    update name in appeal profile constant

--- src/pages/appeals-profile/contants.ts
+++ src/pages/appeals-profile/contants.ts
@@ -2,7 +2,7 @@ import { CandidatesRow } from 'features/vacancy-candidates/types';
 
 export const APPEAl_DATA = {
     id: 1,
-    title: 'Менеджер Проектов',
+    title: 'Frontend-разработка',
     created_at: '10 августа 2022 г.',
     deadline: '10 сентября 2022 г.',
     seats: '10',
