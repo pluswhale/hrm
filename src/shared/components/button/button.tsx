b1a851308af2503e34b398d19a896353b2ff6c91
Your Name <you@example.com>
2024-05-09 02:58:10 +0300

    survey loyaaut fix

--- src/shared/components/button/button.tsx
+++ src/shared/components/button/button.tsx
@@ -2,21 +2,22 @@ import { FC, ReactElement } from 'react';
 import { ButtonProps, dynamicStylesObject } from './types';
 
 export const Button: FC<ButtonProps> = ({
-    text = '',
-    type,
-    image,
-    onClick,
-    styles,
-    disabled,
+                                            view = '',
+                                            type,
+                                            text,
+                                            image,
+                                            onClick,
+                                            styles,
+                                            disabled,
                                             href,
-}): ReactElement => {
+                                        }): ReactElement => {
     const Component = href ? 'a' : 'button';
     return (
         <Component
             href={href} // Add href attribute if it exists
             style={styles || {}}
             disabled={disabled}
-            className={type && dynamicStylesObject[type]}
+            className={view && dynamicStylesObject[view]}
             onClick={onClick}
         >
             {image && <img src={image} alt="" />}
