c791467b968f13556654771481285909e689040d
Egor Dultsev <listenmy@yandex.ru>
2024-05-10 17:17:47 +0300

    fix person card title, change the constants data with normal values

--- src/pages/candidates-list/constants.ts
+++ src/pages/candidates-list/constants.ts
@@ -1,29 +1,29 @@
-import { CandidatesDataContainerProps } from "features/candidates-data-container/types";
-import { FilterSet } from "features/filter/types";
+import { CandidatesDataContainerProps } from 'features/candidates-data-container/types';
+import { FilterSet } from 'features/filter/types';
 
 export const filterSet = [
     {
         id: 1,
-        title: "По должности",
+        title: 'По должности',
         checkboxes: [
-            { label: "Руководство", checked: false },
-            { label: "Менеджер", checked: false },
-        ]
+            { label: 'Руководство', checked: false },
+            { label: 'Менеджер', checked: false },
+        ],
     },
     {
         id: 2,
-        title: "По компетенциям",
+        title: 'По компетенциям',
         checkboxes: [
-            { label: "C#", checked: false },
-            { label: "react", checked: false },
-            { label: "ASP.NET", checked: false },
-            { label: "docker", checked: true },
-            { label: "C#", checked: false },
-            { label: "react", checked: false },
-            { label: "ASP.NET", checked: false },
-            { label: "docker", checked: false }
-        ]
-    }
+            { label: 'C#', checked: false },
+            { label: 'react', checked: false },
+            { label: 'ASP.NET', checked: false },
+            { label: 'docker', checked: true },
+            { label: 'C#', checked: false },
+            { label: 'react', checked: false },
+            { label: 'ASP.NET', checked: false },
+            { label: 'docker', checked: false },
+        ],
+    },
 ] as FilterSet[];
 
 export const candidatesListData = {
@@ -31,31 +31,31 @@ export const candidatesListData = {
         {
             id: 1,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 2,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 3,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 4,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
-    ]
-} as CandidatesDataContainerProps
+    ],
+} as CandidatesDataContainerProps;
 
--- src/pages/employee-list/constants.ts
+++ src/pages/employee-list/constants.ts
@@ -1,27 +1,27 @@
-import { EmployeesDataContainerProps } from "features/employees-list-data-container/types";
-import { FilterSet } from "features/filter/types";
+import { EmployeesDataContainerProps } from 'features/employees-list-data-container/types';
+import { FilterSet } from 'features/filter/types';
 
 export const filterSet = [
     {
         id: 1,
-        title: "По должности",
+        title: 'По должности',
         checkboxes: [
-            { label: "UX/UI designer", checked: true },
-            { label: "Backend-разработчик", checked: false },
-            { label: "Frontend-разработчик", checked: true },
-            { label: "CI/CD", checked: true }
-        ]
+            { label: 'UX/UI designer', checked: true },
+            { label: 'Backend-разработчик', checked: false },
+            { label: 'Frontend-разработчик', checked: true },
+            { label: 'CI/CD', checked: true },
+        ],
     },
     {
         id: 2,
-        title: "По навыкам",
+        title: 'По навыкам',
         checkboxes: [
-            { label: "bootstrap", checked: true },
-            { label: "react", checked: false },
-            { label: "ASP.NET", checked: true },
-            { label: "docker", checked: true }
-        ]
-    }
+            { label: 'bootstrap', checked: true },
+            { label: 'react', checked: false },
+            { label: 'ASP.NET', checked: true },
+            { label: 'docker', checked: true },
+        ],
+    },
 ] as FilterSet[];
 
 export const employeeListData = {
@@ -29,30 +29,30 @@ export const employeeListData = {
         {
             id: 1,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 2,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 3,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
         {
             id: 4,
             imageSrc: 'https://assets.materialup.com/uploads/9e891d99-30d7-4763-9b73-358cc8e8b2cc/preview.png',
-            name: 'Нектов Некто Нектович',
+            name: 'Рыбина Анастасия Алексеевна',
             role: 'Ui/UX designer',
-            skills: ["Figma", "Adobe Illustrator", "Adobe Photoshop", "bootstrap"]
+            skills: ['Figma', 'Adobe Illustrator', 'Adobe Photoshop', 'bootstrap'],
         },
-    ]
-} as EmployeesDataContainerProps
+    ],
+} as EmployeesDataContainerProps;
