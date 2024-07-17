92f9e14d2d5a1bd3e0add7524f555b1f72cdd86c
Egor Dultsev <listenmy@yandex.ru>
2024-07-17 15:24:28 +0300

    added new be

--- src/shared/api/api-config.ts
+++ src/shared/api/api-config.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 import { GetCookie, removeAllCookies, SetCookie } from '../libs/cookies';
 
-const localBackendUrl = 'http://localhost:50000/api/';
+const localBackendUrl = 'http://localhost:4000/';
 
 export const Instance = axios.create({
     baseURL: localBackendUrl,
@@ -28,12 +28,6 @@ const onRequestFulfilled = (config: InternalAxiosRequestConfig) => {
 const onRequestRejected = (error: any) => {
     return Promise.reject(error);
 };
-
-const INVALID_REFRESH_TOKEN_STATUSES = [
-    'Token is invalid or expired',
-    'Token is blacklisted',
-    "Token 'exp' claim has expired",
-];
 
 const onResponseFulfilled = (response: AxiosResponse) => response;
 const onResponseRejected = async (error: any) => {
